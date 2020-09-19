e96456069ea6f8e48ba6c309113f142ed5852a40
Alchimik <lenkov.alm@gmail.com>
2020-09-19 16:09:40 +0800

    SwapiService.js - добавлена трансформация данных при получении

--- src/services/SwapiService.js
+++ src/services/SwapiService.js
@@ -8,7 +8,7 @@ export default class SwapiService {
     const response = await fetch(this._baseUrl + path);
 
     if (!response.ok) {
-      throw new Error(`Could not fetch ${path}, status: ${response.status}`)
+      throw new Error(`Could not fetch ${path}, status: ${response.status}`);
     }
 
     return await response.json();
@@ -16,28 +16,73 @@ export default class SwapiService {
 
   async getAllPeople () {
     const res = await this.getResource(this._peoplePath);
-    return res.results;
+    return res.results.map((item) => this._transformPerson(item));
   }
 
   async getPerson (id) {
-    return this.getResource(this._peoplePath + id + '/');
+    const person = await this.getResource(this._peoplePath + id + '/');
+    return this._transformPerson(person);
   }
 
   async getAllPlanets () {
     const res = await this.getResource(this._planetsPath);
-    return res.results;
+    return res.results.map((item) => this._transformPlanet(item));
   }
 
   async getPlanet (id) {
-    return this.getResource(this._planetsPath + id + '/');
+    const planet = await this.getResource(this._planetsPath + id + '/');
+    return this._transformPlanet(planet);
   }
 
   async getAllStarships () {
     const res = await this.getResource(this._starshipsPath);
-    return res.results;
+    return res.results.map((item) => this._transformStarship(item));
   }
 
   async getStarship (id) {
-    return this.getResource(this._starshipsPath + id + '/');
+    const starship = await this.getResource(this._starshipsPath + id + '/');
+    return this._transformStarship(starship);
+  }
+
+  _extractIdFromUrl (url) {
+    const matches = url.match(/\/(\d+)\/$/);
+    return matches[1];
+  }
+
+  _transformPlanet (planet) {
+    const id = this._extractIdFromUrl(planet.url);
+
+    return {
+      id: id,
+      image_url: `https://starwars-visualguide.com/assets/img/planets/${id}.jpg`,
+      name: planet.name,
+      population: planet.population,
+      rotation_period: planet.rotation_period,
+      diameter: planet.diameter
+    };
+  }
+
+  _transformStarship (starship) {
+    return {
+      id: this._extractIdFromUrl(starship.url),
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: starship.cost_in_credits,
+      length: starship.length,
+      crew: starship.crew,
+      passengers: starship.passengers,
+      cargoCapacity: starship.cargo_capacity
+    };
+  }
+
+  _transformPerson (person) {
+    return {
+      id: this._extractIdFromUrl(person.url),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birth_year,
+      eyeColor: person.eye_color
+    };
   }
 }
