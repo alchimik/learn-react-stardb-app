b4df2ff72298311110704b2525352422e146a6c4
Alchimik <lenkov.alm@gmail.com>
2021-02-14 20:11:46 +0800

    withData - 1) обработка ошибок загрузки.  2) Игнорируем старые загруженные данные

--- src/components/hoc-helpers/withData.js
+++ src/components/hoc-helpers/withData.js
@@ -1,41 +1,68 @@
 import React from 'react';
 import Spinner from '../../components/Spinner';
+import ErrorIndicator from '../ErrorIndicator';
 
 const withData = (View) => {
   return class extends React.Component {
     state = {
-      data: null
+      data: null,
+      loading: true,
+      error: false
     };
 
-    async runDataLoad () {
+    clearEffects = () => {};
+
+    runDataLoad () {
       if (!this.props.getData) {
         return;
       }
 
-      this.setState({ data: null });
-      const data = await this.props.getData();
-      this.setState({ data });
+      let ignoreResult = false;
+
+      const onSuccess = (data) => {
+        !ignoreResult && this.setState({ data, loading: false, error: false });
+      };
+      const onError = () => {
+        !ignoreResult && this.setState({ data: null, loading: false, error: true });
+      };
+
+      this.setState({ data: null, loading: true, error: false });
+      this.props.getData().then(
+        (data) => onSuccess(data),
+        onError
+      );
+
+      return () => ignoreResult = true;
     }
 
     componentDidMount () {
-      this.runDataLoad(this.props.id);
+      this.clearEffects = this.runDataLoad(this.props.id);
     }
 
     componentDidCatch (error, errorInfo) {
-      this.setState({ data: null });
+      this.setState({ data: null, loading: false, error: true });
     }
 
     componentDidUpdate (prevProps, prevState, snapshot) {
       if (this.props.getData !== prevProps.getData) {
-        this.runDataLoad();
+        this.clearEffects();
+        this.clearEffects = this.runDataLoad();
       }
     }
 
-    render () {
-      var { data } = this.state;
+    componentWillUnmount () {
+      this.clearEffects();
+    }
 
-      if (data === null) {
+    render () {
+      var { data, loading, error } = this.state;
+
+      if (loading) {
         return <Spinner/>;
+      }
+
+      if (error) {
+        return <ErrorIndicator/>;
       }
 
       return <View {...this.props} data={data}/>;
