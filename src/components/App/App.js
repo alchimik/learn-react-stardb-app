42f22b8d2a622c14ad33d5d5f860e578eb1afedb
m.lenkov <lenkov.alm@gmail.com>
2022-05-24 19:31:54 +0800

    добавлен StrictMode

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { StrictMode, Component} from 'react';
 import AppHeader from '../AppHeader';
 import ErrorBoundary from '../ErrorBoundary';
 import RandomPlanet from '../RandomPlanet';
@@ -13,39 +13,41 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 import { PersonDetails } from '../SWComponents';
 
-class App extends React.Component {
+class App extends Component {
   render () {
     return (
-      <ErrorBoundary>
-        <SwapiServiceProvider value={new SwapiService()}>
-          <Router>
-            <div className="container">
-              <AppHeader/>
-              <RandomPlanet/>
+      <StrictMode>
+        <ErrorBoundary>
+          <SwapiServiceProvider value={new SwapiService()}>
+            <Router>
+              <div className="container">
+                <AppHeader/>
+                <RandomPlanet/>
 
-              <Route path='/' exact>
-                <h2>Welcome to StarDB</h2>
-              </Route>
+                <Route path="/" exact>
+                  <h2>Welcome to StarDB</h2>
+                </Route>
 
-              <Route path='/people' exact>
-                <PeoplePage/>
-              </Route>
-              <Route path='/people/:id' render={({ match }) => {
-                const { id } = match.params;
-                return <PersonDetails itemId={id}/>;
-              }}/>
+                <Route path="/people" exact>
+                  <PeoplePage/>
+                </Route>
+                <Route path="/people/:id" render={({ match }) => {
+                  const { id } = match.params;
+                  return <PersonDetails itemId={id}/>;
+                }}/>
 
-              <Route path='/planets/:id?'>
-                <PlanetsPage/>
-              </Route>
+                <Route path="/planets/:id?">
+                  <PlanetsPage/>
+                </Route>
 
-              <Route path='/starships/:id?'>
-                <StarshipsPage/>
-              </Route>
-            </div>
-          </Router>
-        </SwapiServiceProvider>
-      </ErrorBoundary>
+                <Route path="/starships/:id?">
+                  <StarshipsPage/>
+                </Route>
+              </div>
+            </Router>
+          </SwapiServiceProvider>
+        </ErrorBoundary>
+      </StrictMode>
     );
   }
 }
